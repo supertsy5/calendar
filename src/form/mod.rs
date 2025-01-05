3dd87d7e2b1c53b3c7081be3bb7436a59a5c2f87
SUPERTSY5 <0x7f178735@gmail.com>
2025-01-05 23:07:36 +0800

    20240105

--- src/form/mod.rs
+++ src/form/mod.rs
@@ -1,18 +1,17 @@
-
 use html::ChildrenProps;
 use yew::prelude::*;
 
 pub use checkbox_input::CheckboxInput;
 pub use color_input::ColorInput;
 pub use int_input::IntInput;
-pub use string_input::StringInput;
 pub use select::{Select, SelectOption};
+pub use string_input::StringInput;
 
 pub mod checkbox_input;
 pub mod color_input;
 pub mod int_input;
-pub mod string_input;
 pub mod select;
+pub mod string_input;
 
 #[function_component]
 pub fn Form(props: &ChildrenProps) -> Html {
